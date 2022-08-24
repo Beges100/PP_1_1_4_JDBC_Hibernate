f738347d517741f97595e34dcd53ccbf64413653
Beges100 <beges200@gmail.com>
2022-08-24 16:35:15 +0300

    realise createUserTable

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,9 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.resource.transaction.spi.TransactionCoordinatorBuilder;
 
 import java.util.List;
 
@@ -12,7 +15,8 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        Session session = Util.getSession();
+        session.beginTransaction();
     }
 
     @Override
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,42 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
+    private static final SessionFactory concreteSessionFactory ;
+
+    static {
+        try {
+            Properties prop = new Properties();
+            prop.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/mydbtest");
+            prop.setProperty("hibernate.connection.username", "root");
+            prop.setProperty("hibernate.connection.password", "Kostya102938");
+            prop.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
+
+            prop.setProperty("hibernate.hbm2ddl.auto", "create");
+
+            concreteSessionFactory = new org.hibernate.cfg.Configuration()
+                    .addProperties(prop)
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+        } catch (Exception ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession()
+            throws HibernateException {
+        return concreteSessionFactory.openSession();
+    }
+
     static Connection connection;
     static String url      = "jdbc:mysql://localhost:3306/";
     private static String dbName   = "mydbtest";
@@ -20,5 +52,4 @@ public class Util {
 
         return connection;
     }
-
 }
