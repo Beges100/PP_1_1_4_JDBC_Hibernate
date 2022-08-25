2c98aeec91ea9fa16d35d9430b4fdbb51b4ed932
Beges100 <beges200@gmail.com>
2022-08-25 15:36:27 +0300

    add try-catch resources

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 import java.util.ArrayList;
@@ -16,59 +17,81 @@ public class UserDaoHibernateImpl implements UserDao {
     public void createUsersTable() {
         String myTableName = "CREATE TABLE IF NOT EXISTS User (id INT NOT NULL AUTO_INCREMENT, " +
                 "name VARCHAR(45) NOT NULL, lastName VARCHAR(45) NOT NULL, age INT NOT NULL, PRIMARY KEY (id))";
-        Session session = Util.getSession();
+        try(Session session = Util.getSession()) {
         session.beginTransaction();
         session.createSQLQuery(myTableName).addEntity(User.class).executeUpdate();
         session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
         String drop = "DROP TABLE IF EXISTS User ";
-        Session session = Util.getSession();
+        try(Session session = Util.getSession()) {
         session.beginTransaction();
         session.createSQLQuery(drop).executeUpdate();
         session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session = Util.getSession();
+        try(Session session = Util.getSession()) {
         session.beginTransaction();
         User user = new User(name,lastName,age);
         session.save(user);
         session.evict(user);
         session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
         String sqlDeleteString = "delete User where id = :param";
-        Session session = Util.getSession();
-        session.beginTransaction();
-        session.createQuery(sqlDeleteString)
-                .setParameter("param", id)
-                .executeUpdate();
-        session.getTransaction().commit();
+        try(Session session = Util.getSession()) {
+            session.beginTransaction();
+            session.createQuery(sqlDeleteString)
+                    .setParameter("param", id)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public List<User> getAllUsers() {
         String sql = "from User";
-        Session session = Util.getSession();
+
+        try(Session session = Util.getSession()) {
         session.beginTransaction();
         List<User> list = session.createQuery(sql).list();
         session.getTransaction().commit();
 
         return list;
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session = Util.getSession();
+        try(Session session = Util.getSession()) {
         session.beginTransaction();
         String hql = String.format("delete from User");
         session.createQuery(hql).executeUpdate();
         session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 }
